95b53198893961a87fd8363a340e0a6e08e6195b
keffi105 <munauwarahkeffi@gmail.com>
2023-11-06 15:20:42 +0100

    exercise 2

---
+++ exercise2.py
@@ -0,0 +1,15 @@
+# if else exercise
+username = 'sani'
+print('what is your username')   # ask for their username
+username = input()
+if username == 'sani':
+    print('what is your password')   # ask for their password
+else:
+    print('unknown username')
+password = input()
+password = '1234'
+if password == '1234':
+    print('assess granted')
+else:
+    print('assess denied')
+print('welcome' + username)
