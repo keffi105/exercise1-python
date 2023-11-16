815123d3e33c4a9f02a432ec4f7f63808a84c012
keffi105 <munauwarahkeffi@gmail.com>
2023-11-16 15:31:56 +0100

    elfi error solve

--- vampire.py
+++ vampire.py
@@ -2,15 +2,16 @@ name = 'alice'
 age = '2'
 print('what is your name')    # ask for their name
 name = input()
+print('what is your age')
+age = input()
 if name == 'alice':
     print('hello alice.')
-    print('what is your age')
-    age = input()
-elif age < 12:
+
+elif int(age) < 12:
     print('You are not Alice, kiddo.')
-elif age > 2000:
+elif int(age) > 2000:
     print('Unlike you, Alice is not an undead, immortal vampire.')
-elif age > 100:
+elif int(age) > 100:
     print('You are not Alice, grannie.')
 else:
     print('you are vampire')
