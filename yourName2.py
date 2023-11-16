6701c931d0027b5f588bc84a8d465a1d5b5eb9ec
keffi105 <munauwarahkeffi@gmail.com>
2023-11-16 16:55:18 +0100

    while statement

---
+++ yourName2.py
@@ -0,0 +1,13 @@
+while True:
+    print('Who are you?')
+    name = input()
+    if name == 'sani':
+      continue
+    print('Hello, Joe. What is the password? (It is a fish.)')
+    password = input()
+    if password == 'sword':
+        break
+print('Access granted.')
+
+
+
