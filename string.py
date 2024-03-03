0ef6f401594a2518fe67bba24a97e67b280e5fbf
keffi105 <munauwarahkeffi@gmail.com>
2024-03-03 23:04:51 +0100

    string

---
+++ string.py
@@ -0,0 +1,9 @@
+print('how are you?')
+feeling = input()
+if feeling.lower() == 'great':
+    print('''that is great 
+    i hope you have a nice day
+    goodbye''')
+else:
+    print('i hope you well be happy')
+
